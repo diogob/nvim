d008141b30587a2f89294f332457f7d639bd5cee
Diogo Biazus <diogo@biazus.ca>
2022-11-20 16:01:11 -0500

    Add rounded borders to LSP popups

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,7 @@
 local opts = { noremap=true, silent=true }
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
