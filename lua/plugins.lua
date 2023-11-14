388e03a6972e7e7fe240c76712427effe91b327c
Diogo Biazus <diogo@biazus.ca>
2023-11-14 10:37:12 -0500

    Reorg plugin loading order to test disabling as much of the config as possible

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -54,6 +54,94 @@ local packadd_defer = function(plugin)
   vim.schedule(function() packadd(plugin) end)
 end
 
+packadd('nvim-cmp')
+packadd('luasnip')
+packadd('cmp_luasnip')
+packadd('cmp-buffer')
+packadd('cmp-nvim-lsp')
+packadd('cmp-path')
+packadd('cmp-emoji')
+
+vim.schedule(function()
+  local cmp = require('cmp')
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'emoji' }, -- For vsnip users.
+      { name = 'buffer' },
+      { name = 'path' },
+    }),
+    enabled = function()
+      local context = require 'cmp.config.context'
+      -- disable command mode completion
+      if vim.api.nvim_get_mode().mode == 'c' then
+        return false
+      else
+        -- disable completion in comments
+        return not context.in_treesitter_capture("comment")
+            and not context.in_syntax_group("Comment")
+      end
+    end
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+end)
+
+packadd_defer('which-key')
+vim.schedule(function()
+  require("which-key").setup({
+    window = {
+      border = "single"
+    }
+  })
+end)
+
 packadd('oil')
 require('oil').setup({
   default_file_explorer = true,
@@ -149,15 +237,6 @@ vim.schedule(function()
   require('telescope').load_extension('textcase')
 end)
 
-packadd_defer('which-key')
-vim.schedule(function()
-  require("which-key").setup({
-    window = {
-      border = "single"
-    }
-  })
-end)
-
 
 packadd_defer('nvim-spectre')
 vim.schedule(function()
@@ -179,85 +258,6 @@ vim.schedule(function()
     view = {
       merge_tool = { layout = 'diff3_mixed' }
     }
-  })
-end)
-
-packadd('nvim-cmp')
-packadd('luasnip')
-packadd('cmp_luasnip')
-packadd('cmp-buffer')
-packadd('cmp-nvim-lsp')
-packadd('cmp-path')
-packadd('cmp-emoji')
-
-vim.schedule(function()
-  local cmp = require('cmp')
-
-  cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-      { name = 'emoji' }, -- For vsnip users.
-      { name = 'buffer' },
-      { name = 'path' },
-    }),
-    enabled = function()
-      local context = require 'cmp.config.context'
-      -- disable command mode completion
-      if vim.api.nvim_get_mode().mode == 'c' then
-        return false
-      else
-        -- disable completion in comments
-        return not context.in_treesitter_capture("comment")
-            and not context.in_syntax_group("Comment")
-      end
-    end
-  })
-
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
-
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
   })
 end)
 
