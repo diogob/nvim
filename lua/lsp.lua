98c7660b2011c4680ce961b7795dd75d928c4dcf
Diogo Biazus <diogo@biazus.ca>
2023-11-28 16:00:51 -0500

    override document formatting from typescript tools since we just want to use prettier

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -90,11 +90,10 @@ lsp.tsserver.setup {
 require("typescript-tools").setup {
   on_attach = on_attach,
   root_dir = lsp.util.root_pattern("package.json"),
-  capabilities = capabilities,
-  init_options = {
-    lint = true,
+  handlers = {
+    ["textDocument/formatting"] = function () end,
+    ["textDocument/rangeFormatting"] = function () end
   },
-  single_file_support = false
 }
 
 lsp.hls.setup {
