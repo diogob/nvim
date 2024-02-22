8833aa4d72ad17099c1ea311729fa03ab6d81366
Diogo Biazus <diogo@biazus.ca>
2024-02-22 11:17:13 -0500

    Testing out conform to replace efm lsp. Also enabled format on save

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -180,7 +180,24 @@ packadd_defer('nvim-code-action-menu')
 packadd_defer('nvim-lightbulb')
 packadd_defer('auto-save')
 
-packadd('efmls-configs-nvim')
+packadd_defer('conform')
+vim.schedule(function()
+  require("conform").setup({
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Use a sub-list to run only the first available formatter
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      typescriptreact = { { "prettierd", "prettier" } },
+    },
+  })
+  vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+end)
 
 packadd_defer('nvim-notify')
 vim.schedule(function()
