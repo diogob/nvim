a1c744cac27808366be7e014d8a8b0e4aadf6829
Diogo Biazus <diogo@biazus.ca>
2024-04-11 09:45:56 -0400

    format on save is akward with auto-save, even with the timeout. I rather have an active role formatting my code.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -188,11 +188,6 @@ end)
 packadd_defer("conform")
 vim.schedule(function()
 	require("conform").setup({
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Use a sub-list to run only the first available formatter
