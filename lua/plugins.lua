9f0eeb59e8438c35cef6cd05b2292eaeb15e8428
Diogo Biazus <diogo@biazus.ca>
2024-10-18 10:21:10 -0400

    just testing signature help. will leave it commented out for now

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -152,16 +152,15 @@ end
 
 packadd_defer("blink.cmp")
 vim.schedule(function()
-  require("blink.cmp").setup(
-    {
-      highlight = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
-        use_nvim_cmp_as_default = true,
-      }
-    }
-  )
+  require("blink.cmp").setup({
+    highlight = {
+      -- sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release, assuming themes add support
+      use_nvim_cmp_as_default = true,
+    },
+    -- trigger = { signature_help = { enabled = true } },
+  })
 end)
 
 packadd_defer("which-key")
