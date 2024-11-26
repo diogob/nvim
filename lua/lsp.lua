5b3df2eacaf7b970f4f525a6ab89d54a727d963f
Diogo Biazus <diogo@biazus.ca>
2024-11-25 22:03:46 -0500

    Tidy up config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,48 +5,23 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, apply_defaults({ desc = "Info" }, bufopts))
   vim.keymap.set("n", "<leader>li", vim.lsp.buf.hover, apply_defaults({ desc = "Info" }, bufopts))
   vim.keymap.set("n", "<leader>lS", vim.lsp.buf.signature_help, apply_defaults({ desc = "Signature help" }, bufopts))
   vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, apply_defaults({ desc = "Signature help" }, bufopts))
+  vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, apply_defaults({ desc = "Format" }, bufopts))
   vim.keymap.set("n", "<leader>lR", function()
     return ":IncRename " .. vim.fn.expand("<cword>")
   end, apply_defaults({ expr = true, desc = "Rename" }, bufopts))
-  vim.keymap.set("n", "<leader>lf", function()
-    vim.lsp.buf.format({ async = true })
-  end, apply_defaults({ desc = "Format" }, bufopts))
-
-  -- workspace
-  vim.keymap.set(
-    "n",
-    "<leader>wa",
-    vim.lsp.buf.add_workspace_folder,
-    apply_defaults({ desc = "Add folder" }, bufopts)
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>wr",
-    vim.lsp.buf.remove_workspace_folder,
-    apply_defaults({ desc = "Remove folder" }, bufopts)
-  )
-  vim.keymap.set("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, apply_defaults({ desc = "List folders" }, bufopts))
 end
 
--- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lsp = require("lspconfig")
 
 lsp.denols.setup({
   on_attach = on_attach,
   root_dir = lsp.util.root_pattern("deno.json*"),
-  -- capabilities = capabilities,
   init_options = {
     lint = true,
     documentFormatting = false,
@@ -58,7 +33,6 @@ vim.g.markdown_fenced_languages = { -- required by denols config
 }
 lsp.eslint.setup({
   on_attach = on_attach,
-  -- capabilities = capabilities,
 })
 
 local function organize_imports()
@@ -73,7 +47,6 @@ end
 lsp.ts_ls.setup({
   on_attach = on_attach,
   root_dir = lsp.util.root_pattern("package.json"),
-  -- capabilities = capabilities,
   init_options = {
     lint = true,
   },
@@ -85,32 +58,7 @@ lsp.ts_ls.setup({
   },
   single_file_support = false,
 })
--- require("typescript-tools").setup({
---   on_attach = on_attach,
---   root_dir = lsp.util.root_pattern("package.json"),
---   handlers = {
---     ["textDocument/formatting"] = function() end,
---     ["textDocument/rangeFormatting"] = function() end,
---   },
---   single_file_support = false,
--- })
-lsp.rust_analyzer.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
 
-lsp.hls.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
-lsp.lua_ls.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
-lsp.solargraph.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
 lsp.tailwindcss.setup({
   on_attach = on_attach,
   root_dir = lsp.util.root_pattern(
@@ -123,17 +71,12 @@ lsp.tailwindcss.setup({
     "postcss.config.mjs",
     "postcss.config.ts"
   ),
-  -- capabilities = capabilities,
 })
-lsp.yamlls.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
-lsp.jsonls.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
-lsp.clangd.setup({
-  on_attach = on_attach,
-  -- capabilities = capabilities,
-})
+
+lsp.rust_analyzer.setup({ on_attach = on_attach })
+lsp.hls.setup({ on_attach = on_attach, })
+lsp.lua_ls.setup({ on_attach = on_attach, })
+lsp.solargraph.setup({ on_attach = on_attach, })
+lsp.yamlls.setup({ on_attach = on_attach, })
+lsp.jsonls.setup({ on_attach = on_attach, })
+lsp.clangd.setup({ on_attach = on_attach, })
