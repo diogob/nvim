9c3c2e464ff0052478f145b315c66f1a143c0a79
Guga Guichard <gustavoguichard@gmail.com>
2024-11-28 14:54:25 -0300

    Highlight when 'yank'

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -41,6 +41,16 @@ require("nvim-treesitter.configs").setup({
 		},
 	},
 })
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 -- Next we configure the opt plugins
 
