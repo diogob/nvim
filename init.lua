59d31fc91b5c392b2f660614e4de81da70178482
Diogo Biazus <diogo.biazus@skyprep.com>
2025-04-16 12:11:51 -0400

    Remove redundant line

--- init.lua
+++ init.lua
@@ -54,7 +54,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.o.tabstop = 4
     vim.o.softtabstop = 4
     vim.o.shiftwidth = 4
-    vim.o.shiftwidth = 4
 
     vim.o.foldmethod = "indent"
     vim.o.foldexpr = "nvim_treesitter#foldexpr()"
